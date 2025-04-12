d2170d2f1bcc825ce1fdc9f804032bec158387a4
Kodak4400 <special0747@gmail.com>
2025-04-13 01:27:12 +0900

    依存関係にAWS SDKを追加し、S3バケットからオブジェクトをリストする非同期関数を実装しました。また、Reactアプリに名前を入力して挨拶する機能を追加し、Viteのバージョンを6.2.3に更新しました。

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,46 @@
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_s3::{config::Region, Client};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn list_objects() -> String {
+    let region_provider = RegionProviderChain::first_try(Region::new("ap-northeast-1"));
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
+
+    let mut response = client
+        .list_objects_v2()
+        .bucket("t-toda.test.bucket")
+        .max_keys(10) // In this example, go 10 at a time.
+        .into_paginator()
+        .send();
+
+    while let Some(result) = response.next().await {
+        match result {
+            Ok(output) => {
+                for object in output.contents() {
+                    println!(" - {}", object.key().unwrap_or("Unknown"));
+                }
+            }
+            Err(err) => {
+                eprintln!("{err:?}")
+            }
+        }
+    }
+
+    format!("Hello! You've been greeted from Rust!")
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, list_objects])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
